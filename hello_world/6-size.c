d7bc60d6ea44ab2ec7714a01d4baa7c8f10292aa
root <hajimursalzade@gmail.com>
2023-10-15 04:49:20 -0700

    6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 int main() {
-	 
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
 	 printf("Size of a char: %lu byte(s)\n", sizeof(char));
 	 printf("Size of an int: %lu byte(s)\n", sizeof(int));
 	 printf("Size of a long int: %lu byte(s)\n", sizeof(long));
