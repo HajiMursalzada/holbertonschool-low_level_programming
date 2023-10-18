c99e438fc0fca282f0e2d36f6601340bb71777bc
root <hajimursalzade@gmail.com>
2023-10-18 12:52:46 -0700

    4

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -6,18 +6,15 @@
  *Return: Always 0 (Success)
  */
 
-int main(void)
-{
-        char ch;
+int main(void) {
+  char ch;
 
-        for (ch = 'a'; ch <= 'z'; ch++)
-	  {
-	    if (ch != 'e' && ch != 'q')
-	      {
-                putchar(ch);
+  for (ch = 'a'; ch <= 'z'; ch++) {
+    if (ch != 'e' && ch != 'q') {
+      putchar(ch);
 
-	      }
-	  }
-        putchar('\n');
-        return (0);
+    }
+  }
+  putchar('\n');
+  return (0);
 }
