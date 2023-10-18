ef59b8e46b2eb2156859c1e7f5390c64055216e3
root <hajimursalzade@gmail.com>
2023-10-18 13:54:08 -0700

    9

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -7,12 +7,13 @@
  *Return: Always 0 (Success)
  */
 
-int main() {
-    int num;
+int main(void)
+{
+  int num;
+  for (num = 0; num <= 9; num++)
+    printf("%d", num);
 
-    for (num = 0; num <= 9; num++)
-      printf("%d", num);
     putchar('\n');
 
-    return 0;
+    return (0);
 }
