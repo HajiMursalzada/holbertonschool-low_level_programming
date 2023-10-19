38bef18c97a230f88809e0f048749a134421c4db
root <hajimursalzade@gmail.com>
2023-10-19 00:37:51 -0700

    3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+
+int _islower(int c);
+{
+  char lower = a;
+  char upper = A;
+  while (lower <= 'z')
+    {
+      _putchar('1');
+      while ( upper <= 'Z')
+	{
+	  _putchar('0');
+	}
+    }
+  _putchar('\n');
+}
