bc0c4783f3890b801659ec5193e608cccc23c3b3
root <hajimursalzade@gmail.com>
2023-10-18 23:51:45 -0700

    1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *
+ *
+ *Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+  char ch = 'a';
+  while(ch <= 'z')
+    {
+      _putchar(ch);
+      ch++;
+    }
+  
+  return (0);
+}
