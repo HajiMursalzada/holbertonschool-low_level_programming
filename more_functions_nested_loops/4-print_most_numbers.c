5e2ab7a5b3ce8eab97d01a8f0075126bda3ab06c
root <hajimursalzade@gmail.com>
2023-10-21 08:07:09 -0700

    4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+File Edit Options Buffers Tools C Help
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers, from 0 to 9, followed by a new line don't print 2 and 4
+ *
+ * Return: nothing
+ */
+
+void print_most_numbers(void)
+{
+        int decimal = 48;
+
+        for (; decimal <= 57 ; decimal++)
+        {
+	  decimal != 50;
+	  decimal != 52;
+	  _putchar(decimal);
+        }
+
+        _putchar('\n');
+}
