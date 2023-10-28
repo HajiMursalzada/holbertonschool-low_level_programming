c6fb0976600e29682804d5551cb6b8a5c2a015d1
root <hajimursalzade@gmail.com>
2023-10-28 08:40:38 -0700

    5

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -3,32 +3,32 @@
 
 void rev_string(char *s)
 {
-  	int i = 1;
-	int dec = *s;
-
-	while (dec)
-	{
-		_putchar(dec);
-		dec = *(s + i);
-		i++;
-	}
-
-	_putchar('\n');
-	i = 0;
-	int revdec = *s;
-	while (revdec)
-	{
-	  i++;
-	  revdec = *(s + i); 
-	}
-	revdec = *s;
-	while (i > 0)
-        {
-
-                i--;
-                revdec = *(s + i);
-                _putchar(revdec);
-        }
-
-        _putchar('\n');
+  int i = 1;
+  int length = 0;
+  int dec = *s;
+  int a = *s;
+  while (dec)
+    {
+      _putchar(dec);
+      dec = *(s + i);
+      i++;
+    }
+  
+  _putchar('\n');
+  
+  while (a)
+    {
+      length++;
+      a = *(s + i); 
+    }
+  a = *s;
+  while (length > 0)
+    {
+      
+      length--;
+      a = *(s + length);
+      _putchar(a);
+    }
+  
+  _putchar('\n');
 }
