50a380f20cca788867271cc666c2932c794cde72
root <hajimursalzade@gmail.com>
2023-10-29 08:31:20 -0700

    c

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+
+
+char *_strstr(char *haystack, char *needle)
+{
+  unsigned int i, j;
+  unsigned int length_needle = strlen(needle);
+
+  for (i = 0; haystack[i] != '\0'; ++i)
+    {
+      
+    }
+}
