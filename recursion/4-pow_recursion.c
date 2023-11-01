75e9d0677b2f80ddeaa6b3fc1ff2722351cff267
root <hajimursalzade@gmail.com>
2023-10-31 23:20:06 -0700

    4

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -3,7 +3,6 @@
 
 int _pow_recursion(int x, int y)
 {
-  int i = 0;
   if (y < 0)
     {
       return (-1);
@@ -12,5 +11,5 @@ int _pow_recursion(int x, int y)
     {
       return (1);
     }
-  return x * _pow_recursion(y);
+  return x * x _pow_recursion(y);
 }
