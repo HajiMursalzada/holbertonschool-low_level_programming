69ef2f105e6ce44e2a6fe152c8087e1f2aad7b23
Haji <hajimursalzade@gmail.com>
2023-12-05 18:45:16 -0800

    singly linked list

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of linked list.
+ * @h: head of linked list.
+ *
+ * Return: the number of nodes.
+ */
+
+size_t print_list(const list_t *h)
+{
+	int a = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d], %s\n", h->len, h->next);
+		h = h->next;
+		a++;
+	}
+	return (a);
+}
