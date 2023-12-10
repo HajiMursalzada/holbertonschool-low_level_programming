0a2770de75cd3fd59cf29353b73544f4278b7a30
Haji <hajimursalzade@gmail.com>
2023-12-10 04:42:15 -0800

    singly

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -9,20 +9,16 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t nc = 0;
+	int sum = 0;
 
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-		{
 			printf("[0] (nil)\n");
-		}
 		else
-		{
-		  printf("[%d] %s\n", h->len, h-> str);
-		}	
-		nc++;
+			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
+		sum++;
 	}
-	return (nc);
+	return (sum);
 }
---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * list_len - counts number of elements in a linked list
+ * @h: linked list
+ * Return: the number of elements
+ */
+
+size_t list_len(const list_t *h)
+{
+	int sum = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		sum++;
+	}
+	return (sum);
+}
