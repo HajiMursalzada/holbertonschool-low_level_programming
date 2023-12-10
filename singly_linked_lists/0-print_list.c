24aa09c041a96f2b6157a4b786408c37cbff4df2
Haji <hajimursalzade@gmail.com>
2023-12-10 04:48:34 -0800

    a

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,12 +1,10 @@
 #include "lists.h"
 
 /**
- * print_list - prints all the elements of linked list.
- * @h: head of linked list.
- *
- * Return: the number of nodes.
+ * print_list - prints all the elements of linked list
+ * @h: head of linked list
+ * Return: number of nodes
  */
-
 size_t print_list(const list_t *h)
 {
 	int sum = 0;
